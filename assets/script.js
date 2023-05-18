ac946e59e555bf9cc9984c325c3ec4ac20906ca7
Maxwell L <mleuthner2002@gmail.com>
2023-05-18 11:40:20 -0500

    Added popup windows and for loop
    Windows feed the variables their values and a loop to pick the type of the next character.

--- assets/script.js
+++ assets/script.js
@@ -12,11 +12,46 @@ function writePassword() {
 
 
 function generatePassword(){
-    var length;
+    var length = 0;
     var lower;
     var upper;
     var numbers;
     var special;
+    var passwordReturn;
+    
+
+    length = prompt("How long would you like you password to be.");
+    while((length < 8) || (128 < length)){
+        length = prompt("Please enter a value between 8-128.");
+    }
+
+    lower = confirm("Would you like your password to contain lowercase.");
+    upper = confirm("Would you like you password to contain uppercase.");
+    numbers = confirm("Woudl you like numbers in you password.");
+    special = confirm("Would you like your password to contain special charcters.");
+
+    for(i = 0; i < length; i++){
+        var type = Math.floor(Math.random() * 4);
+        if(lower == true && type == 0){
+
+            passwordReturn += "0";
+
+        }else if(upper == true && type == 1){
+
+            passwordReturn += "1";
+
+        }else if(numbers == true && type == 2){
+
+            passwordReturn += "2";
+
+        }else if(special == true && type == 3){
+
+            passwordReturn += "3";
+
+        }
+    }
+
+    return passwordReturn;
 }
 
 // Add event listener to generate button
