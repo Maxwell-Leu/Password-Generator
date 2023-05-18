8450fceb55a69cc24f433856ac4fb27686679acb
Maxwell L <mleuthner2002@gmail.com>
2023-05-18 11:54:50 -0500

    Added Char strings
    constant strings have been added and how each character if added to the password

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const lowerString = 'abcdefghijklmnopqrstuvwxyz';
+const upperString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numberString = '0123456789';
+const specialString = ',./;\'[][]`~!@#$%^&*()_+|\"\\';
 
 // Write password to the #password input
 function writePassword() {
@@ -18,7 +22,6 @@ function generatePassword(){
     var numbers;
     var special;
     var passwordReturn;
-    
 
     length = prompt("How long would you like you password to be.");
     while((length < 8) || (128 < length)){
@@ -34,19 +37,19 @@ function generatePassword(){
         var type = Math.floor(Math.random() * 4);
         if(lower == true && type == 0){
 
-            passwordReturn += "0";
+            passwordReturn += lowerString.charAt(Math.floor(Math.random() * lowerString.length));
 
         }else if(upper == true && type == 1){
 
-            passwordReturn += "1";
+            passwordReturn += upperString.charAt(Math.floor(Math.random() * upperString.length));
 
         }else if(numbers == true && type == 2){
 
-            passwordReturn += "2";
+            passwordReturn += numberString.charAt(Math.floor(Math.random() * numberString.length));
 
         }else if(special == true && type == 3){
 
-            passwordReturn += "3";
+            passwordReturn += specialString.charAt(Math.floor(Math.random() * specialString.length));
 
         }
     }
